34d602b6958c5b9a5e57c6c3c42d41afa18fe0cc
keliqiao <qiaokeliqiao@qq.com>
2021-06-01 14:59:59 +0800

    添加项目文件。

---
+++ C++学习案例/C++学习案例.cpp
@@ -0,0 +1,23 @@
+﻿#include "index.h"
+
+class person
+{
+public:
+	char pname[64];
+	int page;
+};
+int main()
+{
+	fstream tempfiles;
+	tempfiles.open("test.txt",ios::in|ios::binary);
+	if(!tempfiles.is_open())
+	{
+		cout<<"文件打开失败"<<endl;
+		return 0;
+	}
+	person pone;
+	tempfiles.read((char*)&pone,sizeof(pone));
+	cout << pone.pname << pone.page << endl;
+	tempfiles.close();
+	return 0;
+}
