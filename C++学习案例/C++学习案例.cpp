d86a4e78e36316e039754816e1599d95c9638732
keliqiao <qiaokeliqiao@qq.com>
2021-06-13 19:30:15 +0800

    类模板的创建与使用

--- C++学习案例/C++学习案例.cpp
+++ C++学习案例/C++学习案例.cpp
@@ -1,46 +1,26 @@
 ﻿#include "index.h"
-
+//创建类模板
+template <class T1,class T2>
 class person
 {
 public:
-	string name;
-	int age;
-	person(string tname,int tage)
+	person(T1 tname,T2 tage)
 	{
 		name = tname;
 		age = tage;
 	}
+	void showperson()
+	{
+		cout << name << " " << age << endl;
+	}
+	T1 name;
+	T2 age;
 };
-template <class T>
-void comparevalue(T temp1,T temp2)
-{
-	if (temp1==temp2)
-	{
-		cout << "相等" << endl;
-	}else
-	{
-		cout << "不相等" << endl;
-	}
-}
-//由于上面的模板不能比较自定义类型person，所以特意将person类型定义好模板
-template<> void comparevalue(person p1,person p2)
-{
-	if (p1.age==p2.age&&p1.name==p2.name)
-	{
-		cout << "同一个人" << endl;
-	}else
-	{
-		cout << "不是同一个人" << endl;
-	}
-}
 int main()
 {
-	int a = 10;
-	int b = 20;
-	comparevalue(a, b);
-	person p1("可", 23);
-	person p2("可", 23);
-	comparevalue(p1, p2);
+	//使用类模板
+	person<string, int> p1("乔大虾", 19);
+	p1.showperson();
 	system("pause");
 	return 0;
 }
