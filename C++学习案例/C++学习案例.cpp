eed0fe7a3aada6e611d6e236e66ccb416b6766e5
keliqiao <qiaokeliqiao@qq.com>
2021-06-13 20:26:52 +0800

    类外的全局函数模板做友元

--- C++学习案例/C++学习案例.cpp
+++ C++学习案例/C++学习案例.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	person<string, int>pone("乔大虾", 20);
 	pone.show();
+	showperson(pone);
 	system("pause");
 	return 0;
 }
--- C++学习案例/template.hpp
+++ C++学习案例/template.hpp
@@ -1,9 +1,21 @@
+#include "index.h"
+template<class t1,class t2>
+class person;
+template<class t1,class t2>
+void showperson(person<t1,t2>ptemp)//类外的全局函数模板
+{
+	cout << "全局姓名：" << ptemp.name << endl;
+	cout << "全局年龄：" << ptemp.age << endl;
+}
 template<class t1, class t2>
 class person
 {
+	friend void showperson<>(person<t1, t2>ptemp);//声明类外实现的全局函数模板做友元
+	
 public:
 	person(t1 tname, t2 tage);
 	void show();
+private:
 	t1 name;
 	t2 age;
 };
