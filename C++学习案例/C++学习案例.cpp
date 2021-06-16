62ccb31b0e7d4cbbeba1fa7e7f423f6de38f298b
keliqiao <qiaokeliqiao@qq.com>
2021-06-16 10:35:41 +0800

    在容器里面存储自定义数据类型的指针，并且通过指针来访问原来的数据

--- C++学习案例/C++学习案例.cpp
+++ C++学习案例/C++学习案例.cpp
@@ -13,16 +13,16 @@ public:
 };
 int main()
 {
-	vector<cperson>vperson;
+	vector<cperson*>vperson;
 	cperson p1("张飞",88);
 	cperson p2("张朝阳",23);
 	cperson p3("张子健",39);
-	vperson.push_back(p1);
-	vperson.push_back(p2);
-	vperson.push_back(p3);
-	for (vector<cperson>::iterator cppoint=vperson.begin();cppoint !=vperson.end();cppoint++ )
+	vperson.push_back(&p1);
+	vperson.push_back(&p2);
+	vperson.push_back(&p3);
+	for (vector<cperson*>::iterator cppoint=vperson.begin();cppoint !=vperson.end();cppoint++ )
 	{
-		cout<<(*cppoint).pname<<(*cppoint).page<<endl;
+		cout<<(*cppoint)->pname<<(*cppoint)->page<<endl;
 	}
 	return 0;
 }
