86e262972eec4dee4999bf900825b5ccd51b3f3f
keliqiao <qiaokeliqiao@qq.com>
2021-07-21 19:59:13 +0800

    list排序案例（如何更好的使用list内置的算法）

--- C++学习案例/C++学习案例.cpp
+++ C++学习案例/C++学习案例.cpp
@@ -1,44 +1,52 @@
 ﻿#include "index.h"
-void printlist1(const list<int>&tl)
-{
-	for (list<int >::const_iterator i=tl.begin();i!=tl.end();i++)
-	{
-		cout<<*i<<" ";
-	}
-	cout<<endl;
-}
 
+class Person
+{
+public:
+	Person(string tname,int tage,int theight)
+	{
+		this->age=tage;
+		this->height=theight;
+		this->name=tname;
+	}
+	string name;
+	int age;
+	int height;
+};
+//指定sort的排序规则
+bool sortfunction(Person&p1,Person&p2)
+{
+	if (p1.age==p2.age)//年龄相同按照身高排序
+	{
+		return p1.height>p2.height;
+	}
+	return p1.age<p2.age;//年龄不同按照年龄排序
+}
 int main()
 {
-	// 创建list链表
-	list<int>l;
-	// 添加数据
-	l.push_back(23);
-	l.push_back(22);
-	l.push_back(24);
-	l.push_back(26);
-	//遍历容器
-	printlist1(l);
-	//区间构造
-	list<int>l1(l.begin(),l.end());
-	//拷贝构造
-	list<int>l2(l);
-	//n个elem
-	list<int>l3(10,223);
-	//赋值
-	list<int>l4;
-	l4=l;
-	list<int>l5;
-	l5.assign(l.begin(),l.end());
-	list<int>l6;
-	l6.assign(10,334);
-	//交换数据
-	cout<<"交换前"<<endl;
-	printlist1(l6);
-	printlist1(l);
-	l6.swap(l);
-	cout<<"交换后"<<endl;
-	printlist1(l6);
-	printlist1(l);
+	list<Person>list1;//创建容器
+	Person p1("刘备", 35 , 175);//准备数据
+	Person p2("曹操", 45 , 180);
+	Person p3("孙权", 40 , 170);
+	Person p4("赵云", 25 , 190);
+	Person p5("张飞", 35 , 160);
+	Person p6("关羽", 35 , 200);
+	list1.push_back(p1);
+	list1.push_back(p2);
+	list1.push_back(p3);
+	list1.push_back(p4);
+	list1.push_back(p5);
+	list1.push_back(p6);
+	for (list<Person>::iterator i=list1.begin();i!=list1.end();i++)
+	{
+		cout<<i->name<<" "<<(*i).age<<" "<<i->height<<endl;
+	}
+	//排序
+	cout<<"排序后"<<endl;
+	list1.sort(sortfunction);
+	for (list<Person>::iterator i=list1.begin();i!=list1.end();i++)
+	{
+		cout<<i->name<<" "<<(*i).age<<" "<<i->height<<endl;
+	}
 	return 0;
 }
