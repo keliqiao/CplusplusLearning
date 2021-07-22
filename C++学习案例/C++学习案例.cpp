58197a9236add8810ebdda614e544cb140d8b44c
keliqiao <qiaokeliqiao@qq.com>
2021-07-22 11:14:08 +0800

    对组的创建与使用

--- C++学习案例/C++学习案例.cpp
+++ C++学习案例/C++学习案例.cpp
@@ -1,22 +1,10 @@
 ﻿#include "index.h"
 int main()
 {
-	set<int>setvessel;
-	pair<set<int>::iterator,bool>returnvalue=setvessel.insert(10);
-	if (returnvalue.second)
-	{
-		cout<<"插入成功"<<endl;
-	}else
-	{
-		cout<<"插入失败"<<endl;
-	}
-	returnvalue=setvessel.insert(10);
-	if (returnvalue.second)
-	{
-		cout<<"插入成功"<<endl;
-	}else
-	{
-		cout<<"插入失败"<<endl;
-	}
+	//对组
+	pair<string,int>pairvalue("乔大虾",22);//对组创建
+	cout<<pairvalue.first<<" "<<pairvalue.second<<endl;//对组的使用
+	pair<string,int>pairvalue1=make_pair("乔大虾1",23);//对组的创建
+	cout<<pairvalue1.first<<" "<<pairvalue1.second<<endl;//对组的使用
 	return 0;
 }
