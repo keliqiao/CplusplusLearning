86e6fa072572af4cccd2aceb533e58076860c8af
keliqiao <qiaokeliqiao@qq.com>
2021-07-24 11:39:43 +0800

    map容器的其他插入数据方式

--- C++学习案例/C++学习案例.cpp
+++ C++学习案例/C++学习案例.cpp
@@ -6,6 +6,9 @@ int main()
 	m.insert(pair<int,int>(1,123));//用匿名的pair数据给map容器赋值
 	m.insert(pair<int,int>(2,343));
 	m.insert(pair<int,int>(3,432));
+	m.insert(make_pair(12,323));//用make pair制造对组
+	m.insert(map<int,int>::value_type(5,23));//用内置函数进行插值
+	m[12]=1233;//用中括号中的值代表key，等号后面的代表value，进行插入
 	for (map<int,int>::iterator i=m.begin();i!=m.end();i++)
 	{
 		cout<<i->first<<" "<<(*i).second<<endl;
