88b05d1d06229a93cf20543d27c867bab73acc61
keliqiao <qiaokeliqiao@qq.com>
2021-07-27 11:26:27 +0800

    一元谓词

--- C++学习案例/C++学习案例.cpp
+++ C++学习案例/C++学习案例.cpp
@@ -1,30 +1,22 @@
 ﻿#include "index.h"
-
+//返回值类型为bool的仿函数为谓词，参数只有一个叫一元谓词，参数两个叫两元谓词
 class person
 {
 public:
-	person()
+	bool operator()(int val)
 	{
-		this->countunm=0;
+		return val>5;
 	}
-	int operator()(int a,int b)//定义函数对象，本质上是重载了
-	{
-		this->countunm++;
-		return a+b;
-	}
-	int countunm;//函数对象自己记录自己调用的次数
 };
-int seconduse(person&tpone,int a,int b)
-{
-	return tpone(a,b);
-}
 int main()
 {
-	person pone;//创建普通对象，其实可以当做函数对象
-	cout<<pone(123,34)<<endl;//使用函数对象，操作函数对象使用重载的小括号
-	cout<<pone(123,34)<<endl;//使用函数对象，操作函数对象使用重载的小括号
-	cout<<pone(123,34)<<endl;//使用函数对象，操作函数对象使用重载的小括号
-	cout<<seconduse(pone,123,34)<<endl;//使用函数对象作为函数参数
-	cout<<"函数对象已经调用了"<<pone.countunm<<"次"<<endl;
+	vector<int>ve;
+	for (int i=0;i<10;i++)
+	{
+		ve.push_back(i);
+	}
+	//person（）是一个匿名对象
+	vector<int>::iterator it= find_if(ve.begin(),ve.end(),person());//使用用一元谓词,find if返回的是一个迭代器相当于指针
+	cout<<*it<<endl;
 	return 0;
 }
