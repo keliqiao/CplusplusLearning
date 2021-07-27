1ff57ef07149882362f63f3cd8a11b5252d245d2
keliqiao <qiaokeliqiao@qq.com>
2021-07-27 11:12:50 +0800

    函数对象，本质上是类中重载了小括号

--- C++学习案例/C++学习案例.cpp
+++ C++学习案例/C++学习案例.cpp
@@ -1,77 +1,30 @@
 ﻿#include "index.h"
-class Worker
+
+class person
 {
 public:
-	string m_Name;
-	int m_Salary;
+	person()
+	{
+		this->countunm=0;
+	}
+	int operator()(int a,int b)//定义函数对象，本质上是重载了
+	{
+		this->countunm++;
+		return a+b;
+	}
+	int countunm;//函数对象自己记录自己调用的次数
 };
-void creatvWorker(vector<Worker>&t_vector)
+int seconduse(person&tpone,int a,int b)
 {
-	string nameSeed="ABCDEFGHIJ";
-	for (int i=0;i<10;i++)
-	{
-		Worker worker;
-		worker.m_Name="员工";
-		worker.m_Name+=nameSeed[i];
-		worker.m_Salary=rand()%1000+3000;
-		t_vector.push_back(worker);
-	}
-}
-void setGroup(vector<Worker>&t_vector,multimap<string,Worker>&t_multimap)
-{
-	string randstring[3]={"策划","管理","执行"};
-	for (vector<Worker>::iterator i=t_vector.begin();i!=t_vector.end();i++)
-	{
-		//产生部门随机编号
-		int deptid=rand()%3;
-		string dept=randstring[deptid];
-		//将员工插入到map容器中
-		t_multimap.insert(make_pair(dept,*i));
-	}
-}
-void showWorker(multimap<string,Worker>&t_multimap)
-{
-	cout<<"策划部门↓"<<endl;
-	cout<<"================="<<endl;
-	multimap<string,Worker>::iterator pos=t_multimap.find("策划");
-	int countnum=t_multimap.count("策划");//找到策划部门的所有人数
-	int index=0;
-	for (multimap<string,Worker>::iterator i=pos;i!=t_multimap.end()&&index<countnum;i++,index++)//如果已经找到的人数超过策划部门的所有人数就终止循环
-	{
-		cout<<"姓名 "<<i->second.m_Name<<" 工资 "<<i->second.m_Salary<<endl;
-	}
-	cout<<"================="<<endl;
-	cout<<"管理部门↓"<<endl;
-	cout<<"================="<<endl;
-	pos=t_multimap.find("管理");
-	countnum=t_multimap.count("管理");//找到管理部门的所有人数
-	index=0;
-	for (multimap<string,Worker>::iterator i=pos;i!=t_multimap.end()&&index<countnum;i++,index++)//如果已经找到的人数超过策划部门的所有人数就终止循环
-	{
-		cout<<"姓名 "<<i->second.m_Name<<" 工资 "<<i->second.m_Salary<<endl;
-	}
-	cout<<"================="<<endl;
-	cout<<"执行部门↓"<<endl;
-	cout<<"================="<<endl;
-	pos=t_multimap.find("执行");
-	countnum=t_multimap.count("执行");//找到执行部门的所有人数
-	index=0;
-	for (multimap<string,Worker>::iterator i=pos;i!=t_multimap.end()&&index<countnum;i++,index++)//如果已经找到的人数超过策划部门的所有人数就终止循环
-	{
-		cout<<"姓名 "<<i->second.m_Name<<" 工资 "<<i->second.m_Salary<<endl;
-	}
-	cout<<"================="<<endl;
+	return tpone(a,b);
 }
 int main()
 {
-	srand((unsigned int)time(NULL));
-	//创建员工
-	vector<Worker>vWorker;
-	creatvWorker(vWorker);
-	//员工分组
-	multimap<string,Worker>mWorker;
-	setGroup(vWorker,mWorker);
-	//显示员工
-	showWorker(mWorker);
+	person pone;//创建普通对象，其实可以当做函数对象
+	cout<<pone(123,34)<<endl;//使用函数对象，操作函数对象使用重载的小括号
+	cout<<pone(123,34)<<endl;//使用函数对象，操作函数对象使用重载的小括号
+	cout<<pone(123,34)<<endl;//使用函数对象，操作函数对象使用重载的小括号
+	cout<<seconduse(pone,123,34)<<endl;//使用函数对象作为函数参数
+	cout<<"函数对象已经调用了"<<pone.countunm<<"次"<<endl;
 	return 0;
 }
